2d35dcde8bedc7ff5d0bdb119b3f9c666005de60
AdamWudarczyk1234 <53154193+AdamWudarczyk1234@users.noreply.github.com>
2019-11-07 12:46:08 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,3 +39,33 @@ function showError(error) {
   notificationElement.style.display = "block";
   notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
+
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude) {
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+  fetch(api)
+    .then(function(response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+      weather.city = data.name;
+      weather.country = data.sys.country;
+    })
+    .then(function() {
+      displayWeather();
+    });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather() {
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
